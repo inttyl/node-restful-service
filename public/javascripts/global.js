191921673beabb3c934977ddda1de0a93b7d8e6c
curvenut <ltyqcca@gmail.com>
2016-07-12 00:25:06 -0400

    'suite'

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -8,6 +8,8 @@ $(document).ready(function(){
 
     populateTable();
 
+    $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+
 });
 
 
@@ -15,16 +17,42 @@ function populateTable(){
 
     var tableContent='';
 
+
     $.getJSON('/users/userlist', function(data){
+
+        userListData = data;
 
         $.each(data, function(){
             tableContent += '<tr>';
-            tableContent += '<td> <a href="#" class="linkshowuser" rel="' + this.username + '">' +'</a> </td>';
+            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
             tableContent += '<td>' + this.email + '</td>';
-            tableContent += '<td> <a href="#" class="linkdeleteuser" rel="' + this.id+  '">'+ delete + '</a> </td>';
+            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
             tableContent += '</tr>';
         });
+        $('#userList table tbody').html(tableContent);
 
-        $('#userlist table tbody').html(tableContent);
     });
+}
+
+
+function showUserInfo(event){
+
+    // Prevent Link from Firing
+    event.preventDefault();
+
+    // Retrieve username from link rel attribute
+    var thisUserName = $(this).attr('rel');
+
+    // Get Index of object based on id value
+    var arrayPosition = userListData.map(function(arrayItem) {return arrayItem.username; }).indexOf(thisUserName);
+
+    // Get our User Object
+    var thisUserObject = userListData[arrayPosition];
+
+    //Populate Info Box
+    $('#userInfoName').text(thisUserObject.fullname);
+    $('#userInfoAge').text(thisUserObject.age);
+    $('#userInfoGender').text(thisUserObject.gender);
+    $('#userInfoLocation').text(thisUserObject.location);
+
 }
