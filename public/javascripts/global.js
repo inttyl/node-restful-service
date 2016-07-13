c9c4d3870e44827bf1659cd29c19fffb184460ec
curvenut <ltyqcca@gmail.com>
2016-07-12 23:22:33 -0400

    'suite'

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -10,6 +10,8 @@ $(document).ready(function(){
 
     $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
 
+    $('#addUser button #btnAddUser').on('click', addUser);
+
 });
 
 
@@ -54,5 +56,39 @@ function showUserInfo(event){
     $('#userInfoAge').text(thisUserObject.age);
     $('#userInfoGender').text(thisUserObject.gender);
     $('#userInfoLocation').text(thisUserObject.location);
+
+}
+
+
+function addUser(event){
+
+    var errorCount = 0;
+    var newUser;
+
+    event.preventDefault();
+
+    //VErifie validite du formulaire
+    $('#addUser input').each(function(index, val){
+        if($(this).val() === '') {
+            errorCount++;
+        }
+    });
+
+    if(errorCount === 0 ){
+        user = {
+            'username': $('#addUser fieldset input#inputUserName').val(),
+            'email':$('#addUser fieldset input#inputUserEmail').val(),
+            'fullname':$('#addUser fieldset input#inputUserFullname').val(),
+            'age':$('#addUser fieldset input#inputUserAge').val(),
+            'location':$('#addUser fieldset input#inputUserLocation').val(),
+            'gender':$('#addUser fieldset input#inputUserGender').val()
+        };
+
+        
+
+    }else{
+        alert("Complete tout le formulaire");
+        return false;
+    }
 
 }
--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,16 @@ router.get('/userlist', function(req, res) {
     });
 });
 
+
+/*
+ * Add user
+ */
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.insert(req.body, function(err, result){
+        res.send( (err === null) ? {msg:''} : {msg : err});
+    });
+});
+
 module.exports = router;
